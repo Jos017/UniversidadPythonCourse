07ec3656ebadd55e2e8f1d18772f0386d08b25a8
Jose Rios <jrbernabe@gmail.com>
2023-10-17 17:18:15 -0400

    Add dictionary data type example

---
+++ 03_Data_Types/dictionaries.py
@@ -0,0 +1,7 @@
+# A dictionary (dict) is defined by using double brackets ({})
+# A dictionary has items presented in key:value pairs, and can be referred to by using the key name.
+my_dict = {
+    'name': 'user',
+    'email': 'user@email.com'
+}
+print(my_dict, type(my_dict))
