7fdeb8be984afdf89c5c5ef55076a2b94636aa47
Jose Rios <jrbernabe@gmail.com>
2023-10-17 17:22:57 -0400

    Add binaries data type examples

---
+++ 03_Data_Types/binaries.py
@@ -0,0 +1,15 @@
+# pylint: disable=invalid-name
+
+# Binary data is divided into three types
+
+# 1. bytes (bytes)
+my_bytes = b'Hello'
+print(my_bytes, type(my_bytes))
+
+# 2. bytearray (bytearray)
+my_bytearray = bytearray(5)
+print(my_bytearray, type(my_bytearray))
+
+# 3. memoryview (memoryview)
+my_memoryview = memoryview(bytes(5))
+print(my_memoryview, type(my_memoryview))
