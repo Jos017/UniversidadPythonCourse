02ef0da686cd3eecf2aba0716f3728e40f7c40f5
Jose Rios <jrbernabe@gmail.com>
2023-10-17 17:40:22 -0400

    Add concatenate strings example

---
+++ 04_Strings/concatenate.py
@@ -0,0 +1,8 @@
+#pylint: disable=invalid-name
+
+# A string can be concatenated
+
+# Using (+) to concatenate
+# When using (+) to concatenate the values must be strings
+email = 'user@email.com'
+print('My email is: ' + email)
