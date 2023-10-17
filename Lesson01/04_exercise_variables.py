3d9956727deff6d65c8bd6996ecad0beea6e3830
Jose Rios <jrbernabe@gmail.com>
2023-10-17 15:07:47 -0400

    Add variables exercise

---
+++ Lesson01/04_exercise_variables.py
@@ -0,0 +1,8 @@
+# Declare 3 variables, name, phone, and email
+name = 'user01'
+phone = 123123123
+email = 'user01@email.com'
+
+print('Name: ', name)
+print('Phone: ', phone)
+print('Email: ', email)
