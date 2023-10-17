d3917160b8127cfe7d34b29d874cb628a0a111c2
Jose Rios <jrbernabe@gmail.com>
2023-10-17 15:38:52 -0400

    Add data type numbers examples

---
+++ 03_Data_Types/01_numbers.py
@@ -0,0 +1,15 @@
+# Use type(data) to obtain the data type
+
+# Numeric data is divided into three types
+
+# 1. Integers (int)
+intNumb = 1
+print(intNumb, type(intNumb))
+
+# 2. Float (float)
+floatNumb = 1.0
+print(floatNumb, type(floatNumb))
+
+# 3. Complex (int)
+complexNumb = 1 + 1j
+print(complexNumb, type(complexNumb))
