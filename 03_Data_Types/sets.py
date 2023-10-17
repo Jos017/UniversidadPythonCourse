fad0b0132d7fc25a6720fa9fc142ea8e3509a845
Jose Rios <jrbernabe@gmail.com>
2023-10-17 17:25:41 -0400

    Add sets data type examples

---
+++ 03_Data_Types/sets.py
@@ -0,0 +1,9 @@
+# Set data is divided into two types
+
+# 1. Set (set)
+my_set = {'apple', 'banana', 'cherry'}
+print(my_set, type(my_set))
+
+# 2. Frozenset (frozenset)
+my_frozenset = frozenset({'apple', 'banana', 'cherry'})
+print(my_frozenset, type(my_frozenset))
