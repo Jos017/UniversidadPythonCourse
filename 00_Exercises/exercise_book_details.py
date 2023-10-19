464d95f9f8acabc5ef96f854dcf5e0d1366d2950
Jose Rios <jrbernabe@gmail.com>
2023-10-19 11:42:23 -0400

    Add bokk details exercise

---
+++ 00_Exercises/exercise_book_details.py
@@ -0,0 +1,11 @@
+# Ask for a Book info (title, author)
+# Print the info in the following format:
+# Write the title:
+# Write the author:
+# <title> was written by <author>
+
+title = input('Write a book title: ')
+author = input('Who is the author?: ')
+response = title + ' was written by ' + author
+
+print(response)
