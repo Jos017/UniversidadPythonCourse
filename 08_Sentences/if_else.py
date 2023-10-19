07f84c149cb7c688f4b5f7fb0b7331781c4dab3e
Jose Rios <jrbernabe@gmail.com>
2023-10-19 17:49:42 -0400

    Add ternary operator example

--- 08_Sentences/if_else.py
+++ 08_Sentences/if_else.py
@@ -23,3 +23,10 @@ elif number < 10:
     print('The number is lower than 10 but more or equal than 5')
 else:
     print('The number is higher than 10')
+
+# We can simplify an if else sentence using the ternary operator
+# this works in one line
+# Syntax: <code_true> if <condition> else <code_false>
+# The result can be assigned
+a = print('The condition is True') if 5 > 2 else print('The condition is False')
+b = print('The condition is True') if 2 > 5 else print('The condition is False')
