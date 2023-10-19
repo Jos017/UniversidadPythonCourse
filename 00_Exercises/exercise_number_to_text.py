fe39f5bee5aff2c8040a7db2e838f48f5bd13124
Jose Rios <jrbernabe@gmail.com>
2023-10-19 17:41:31 -0400

    Add number to text exercise

---
+++ 00_Exercises/exercise_number_to_text.py
@@ -0,0 +1,13 @@
+# Transform a number between 1 and 3 to its text name
+# One, two three
+
+number = int(input('Introduce a number between 1 to 3: '))
+
+if number == 1:
+    print('The number is One')
+elif number == 2:
+    print('The number is Two')
+elif number == 3:
+    print('The number is Three')
+else:
+    print('The number is invalid')
