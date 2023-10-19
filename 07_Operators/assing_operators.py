177e94c1ce519f29cc9d1c59ca3106032dd726f1
Jose Rios <jrbernabe@gmail.com>
2023-10-19 14:29:34 -0400

    Add assign operators examples

---
+++ 07_Operators/assing_operators.py
@@ -0,0 +1,35 @@
+# Assign operators allows us to assign values to variables
+
+# Assign (=)
+my_variable = 1
+print(my_variable)  # ==> 1
+
+# we can use a variable it self to reassign its value
+my_variable = my_variable + 1
+print(my_variable)  # ==> 2
+
+# Concatenate Assign (+=) with strings
+my_variable = 'Hello'
+my_variable += ' world'
+print(my_variable)  # ==> 'Hello world'
+
+
+# Addition Assign (+=)
+my_variable = 1
+my_variable += 5
+print(my_variable)  # ==> 6
+
+# Subtraction Assign (-=)
+my_variable = 1
+my_variable -= 5
+print(my_variable)  # ==> -4
+
+# Multiplication Assign (-=)
+my_variable = 2
+my_variable *= 5
+print(my_variable)  # ==> 10
+
+# Division Assign (/=)
+my_variable = 10
+my_variable /= 2
+print(my_variable)  # ==> 5
