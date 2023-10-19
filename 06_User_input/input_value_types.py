7e0fb62baf5235573c03f5e48cce0ed93d953cf9
Jose Rios <jrbernabe@gmail.com>
2023-10-19 11:20:22 -0400

    Add input return value example

---
+++ 06_User_input/input_value_types.py
@@ -0,0 +1,9 @@
+# Input return value type
+# The input function always returns a string,
+# so we have to transform the type to handle them if we don't want a string
+
+number_1 = int(input('Write the first number: '))
+number_2 = int(input('Write the second number: '))
+result = number_1 + number_2
+
+print('The sum is: result')
