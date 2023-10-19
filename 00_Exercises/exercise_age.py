c9d870b3e837f0c3ce0ad714715a981e65427903
Jose Rios <jrbernabe@gmail.com>
2023-10-19 14:53:26 -0400

    Add adult or not exercise

---
+++ 00_Exercises/exercise_age.py
@@ -0,0 +1,9 @@
+# Exercise that returns if a person is adult or not
+
+print('Adult or not calculator')
+age = int(input('Introduce your age: '))
+
+if age >= 18:
+    print('You are an Adult')
+else:
+    print('You are NOT an Adult')
