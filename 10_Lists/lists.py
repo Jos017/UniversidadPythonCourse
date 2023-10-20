a1bbc008f4dcc03513e6c8f78984c739d2b9761a
Jose Rios <jrbernabe@gmail.com>
2023-10-20 10:17:36 -0400

    Add list basics example

---
+++ 10_Lists/lists.py
@@ -0,0 +1,15 @@
+# A list in python is declared with square brackets []
+names = ['Juan', 'Karla', 'Ricardo', 'Maria']
+
+# A list can have items of different type
+mixed_list = [1, 'Juan', True, ['hello', 'world']]
+
+# Every element in the list has its index
+# The index starts in 0
+# You can access the items in a list by writing: list[index]
+print(names[0]) # ==> Juan
+print(names[1]) # ==> Karla
+print(names[2]) # ==> Ricardo
+print(names[3]) # ==> Maria
+# If we try to access a list item with a non existing index, we will get an error
+# print(names[4])
