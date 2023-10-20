e31a0c2ad5978cb694cbcfa99af2b46d0b6c6747
Jose Rios <jrbernabe@gmail.com>
2023-10-20 09:58:14 -0400

    Add loop for example

---
+++ 09_Loops/for.py
@@ -0,0 +1,24 @@
+# The loop for allows to execute a code a determined times
+
+# for in
+# for in allows to iterate over a string or list or ranges
+# you can use every value iterated in the code
+# Syntax:
+# for <value> in <toIterate>
+my_string = 'Hello'
+my_list = ['apple', 'banana', 'cherry']
+my_range = range(1, 6)
+
+print('String loop for')
+for char in my_string:
+    print(char)
+
+print('List loop for')
+for item in my_list:
+    print(item)
+
+print('Range loop for')
+for numb in my_range:
+    print(numb)
+
+print('loop for has ended')
