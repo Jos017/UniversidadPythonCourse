2ff251e7d884ba78d4f21a3d8104df521c637677
Jose Rios <jrbernabe@gmail.com>
2023-10-20 11:28:52 -0400

    Add tuples example

---
+++ 11_Tuples/tuples.py
@@ -0,0 +1,23 @@
+# A Tuple is defined by parenthesis ()
+# A Tuple works the same way as a List
+# The difference is that a Tuple is not mutable
+
+########
+# If a tuple only has one element you should use a comma after this element
+# This is know that is a tuple
+# single_item_tuple = ('Item',)
+
+fruits = ('Orange', 'Banana', 'Papaya')
+print(fruits)
+
+# tuple length
+print(len(fruits))  # ==> 3
+
+# access an item
+print(fruits[0])  # ==> Orange
+
+# access an item inverse
+print(fruits[-1])  # ==> Papaya
+
+# access multiple items
+print(fruits[0:2])  # ==> (Orange', 'Banana')
