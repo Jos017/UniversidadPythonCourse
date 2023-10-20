eab9c135869dd685f13c53c2328415fd23fcca9b
Jose Rios <jrbernabe@gmail.com>
2023-10-20 11:32:06 -0400

    Add for loop with tuples example

--- 09_Loops/for.py
+++ 09_Loops/for.py
@@ -1,13 +1,14 @@
 # The loop for allows to execute a code a determined times
 
 # for in
-# for in allows to iterate over a string or list or ranges
+# for in allows to iterate over any iterable (string, list, range, tuple)
 # you can use every value iterated in the code
 # Syntax:
 # for <value> in <toIterate>
 my_string = 'Hello'
 my_list = ['apple', 'banana', 'cherry']
 my_range = range(1, 6)
+my_tuple = ('apple', 'banana', 'cherry')
 
 print('String loop for')
 for char in my_string:
@@ -20,5 +21,9 @@ for item in my_list:
 print('Range loop for')
 for numb in my_range:
     print(numb)
+
+print('Tuple loop for')
+for item in my_tuple:
+    print(item)
 
 print('loop for has ended')
