4fa01bccadd00970756a3a83d1380547af89d637
Jose Rios <jrbernabe@gmail.com>
2023-10-23 15:35:52 -0400

    Add modules example

---
+++ 15_POO/Person.py
@@ -0,0 +1,41 @@
+# A module has property __name__, to know the module name
+
+# Class Person
+class Person:
+    def __init__(self, name, last_name, age):
+        self._name = name
+        self._last_name = last_name
+        self._age = age
+
+    @property  # Decorator, allows the getter methods behaves as an attribute
+    def name(self):  # Getter method
+        print(f'Calling get method name: {self._name}')
+        return self._name
+
+    @name.setter  # Decorator, allows the setter method behaves as an attribute
+    def name(self, name):  # Setter method
+        print(f'Calling set method name with: {name}')
+        self._name = name
+
+    @property  # Decorator, allows the getter methods behaves as an attribute
+    def last_name(self):  # Getter method
+        print(f'Calling get method last_name: {self.last_name}')
+        return self._last_name
+
+    @last_name.setter  # Decorator, allows the setter method behaves as an attribute
+    def last_name(self, last_name):  # Setter method
+        print(f'Calling set method last_name with: {last_name}')
+        self._last_name = last_name
+
+    @property  # Decorator, allows the getter methods behaves as an attribute
+    def age(self):  # Getter method
+        print(f'Calling get method age: {self._age}')
+        return self._age
+
+    @age.setter  # Decorator, allows the setter method behaves as an attribute
+    def age(self, age):  # Setter method
+        print(f'Calling set method age with: {age}')
+        self._age = age
+
+    def show_details(self):
+        print(f'Person: {self._name} {self._last_name} {self._age}')
---
+++ 15_POO/modules.py
@@ -0,0 +1,11 @@
+# A module is a file containing Python definitions and statements
+# A module name is the file name
+
+# To import everything from a module use *
+# from Person import *
+
+# Import required
+from Person import Person
+
+person_1 = Person('Karla', 'Gomez', 24)
+person_1.show_details()
