712af87a430f0b333b96b15ad98e25c72099f019
Jose Rios <jrbernabe@gmail.com>
2023-10-25 08:36:09 -0400

    Add constants examples

---
+++ 16_Constants/constants.py
@@ -0,0 +1,10 @@
+# In python we don't have a way to create constants
+# But there's a convention to write constants
+# 1. Should be named with uppercase and snake case
+# 2. Should be imported from a file
+
+MY_CONSTANT = 'This is a constant'
+
+
+class Mathematics:
+    PI = 3.1416
---
+++ 16_Constants/try_constants.py
@@ -0,0 +1,5 @@
+from constants import MY_CONSTANT
+from constants import Mathematics as Mate
+
+print(MY_CONSTANT)
+print(Mate.PI)
