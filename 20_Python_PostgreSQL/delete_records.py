d19891436230c7ace4c62f8990341b477d82d9d9
Jose Rios <jrbernabe@gmail.com>
2023-10-27 16:13:22 -0400

    Add delete record example

---
+++ 20_Python_PostgreSQL/delete_records.py
@@ -0,0 +1,41 @@
+import psycopg
+
+try:
+    with psycopg.connect(user='postgres', password='admin', dbname='test_db') as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('DROP TABLE IF EXISTS persons')
+
+            cursor.execute('''
+                CREATE TABLE persons (
+                    person_id serial PRIMARY KEY,
+                    name varchar,
+                    last_name varchar,
+                    email varchar
+                )
+            ''')
+
+            values = (
+                ('Marcos', 'Cantu', 'mcantu@email.com'),
+                ('Angel', 'Quintana', 'aquintana@email.com'),
+                ('Maria', 'Gonzalez', 'mgonzalez@email.com'),
+            )
+
+            cursor.executemany(
+                'INSERT INTO persons (name, last_name, email) VALUES (%s, %s, %s)',
+                values
+            )
+
+            # Delete a record from table persons
+            sentence = 'DELETE FROM persons WHERE person_id=(%s)'
+            user_input = input('Provide a person_id to delete: ')
+            cursor.execute(sentence, (user_input,))
+            # Number of records affected by the precedent sentence
+            updated_records = cursor.rowcount
+            print(f'Deleted Records: {updated_records}')
+
+            cursor.execute('SELECT * FROM persons ORDER BY person_id ASC')
+            records = cursor.fetchall()
+            print(records)
+
+except psycopg.DatabaseError as e:
+    print(f'Error: {e}')
