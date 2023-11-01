f0e43abe7099b07d23a71b80d65ef30ebf0b414c
Jose Rios <jrbernabe@gmail.com>
2023-11-01 17:40:10 -0400

    Add align strings examples

---
+++ 04_Strings/align_strings.py
@@ -0,0 +1,14 @@
+# Align strings
+
+
+# center - Center a string
+text = 'This is centered'
+print(text.center(50, '-'))
+
+# ljust - Justify left
+text = 'This is left'
+print(text.ljust(50, '-'))
+
+# rjust - Justify right
+text = 'This is right'
+print(text.rjust(50, '-'))
