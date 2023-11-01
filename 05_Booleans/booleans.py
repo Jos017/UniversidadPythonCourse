19548d702dcf02bb5ab3e1178983cc094cab3539
Jose Rios <jrbernabe@gmail.com>
2023-11-01 17:55:32 -0400

    Add Falsy values

--- 05_Booleans/booleans.py
+++ 05_Booleans/booleans.py
@@ -1,3 +1,15 @@
+'''
+Eight ways to obtain a bool False in Python
+1. Empty string ("", '')
+2. Empty List []
+3. Empty Tuple ()
+4. Empty dictionary {}
+5. Int Zero 0
+6. Float Zero 0.0
+7. bool False
+8. None
+'''
+
 # A boolean can be True or False
 my_variable = True
 print(my_variable)
