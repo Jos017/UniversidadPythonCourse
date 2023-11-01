abd648da5ca78556d37d3b5b4e36e46a74abcc18
Jose Rios <jrbernabe@gmail.com>
2023-11-01 17:55:49 -0400

    Add none examples

--- 03_Data_Types/none.py
+++ 03_Data_Types/none.py
@@ -2,3 +2,9 @@
 # It is used to define a null value, or no value at all
 my_none = None
 print(my_none, type(my_none))
+
+# None is considered false
+if my_none:
+    print('Is True')
+else:
+    print('Is False')
