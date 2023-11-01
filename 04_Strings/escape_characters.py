6c9e44003493d3d7bca619100c50e969276fe470
Jose Rios <jrbernabe@gmail.com>
2023-11-01 10:07:21 -0400

    Add escape characters examples

---
+++ 04_Strings/escape_characters.py
@@ -0,0 +1,21 @@
+# Escape characters
+result = "This is a single quote(')"
+print(result)
+result = 'This is a single quote(\')'
+print(result)
+
+# Backspace (\b) removes a character
+result = 'Letter g is going\b to be removed.'
+print(result)
+
+# next row
+result = 'Letter g is a\nnext row.'
+print(result)
+
+# Directories windows
+result = 'c:\\new\\juan'
+print(result)
+
+# row string (Ignores special characters just write as regular characters)
+result = r'Letter g is a\nnext row.'
+print(result)
