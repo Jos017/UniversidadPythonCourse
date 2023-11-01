5273881b9a267a6e038916b22d7a59b113cf7679
Jose Rios <jrbernabe@gmail.com>
2023-11-01 07:52:03 -0400

    Add numeral systems example

---
+++ 03_Data_Types/numeric/numeral_systems.py
@@ -0,0 +1,21 @@
+# Numeral systems
+
+# Decimal
+a = 10
+b = int('23', base=10)
+
+# Binary
+a = 0b1010
+b = int('10111', base=2)
+
+# Octal
+a = 0o12
+b = int('27', base=8)
+
+# Hexadecimal
+a = 0xA
+b = int('17', base=16)
+
+# When print a, it prints as decimal
+print(f'{a}')
+print(f'{b}')
